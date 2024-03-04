f2967c6fc889036de1880fce93a7dc3f1e060fca
Ankit Sharma <ankitsharma11876@gmail.com>
2024-03-04 17:10:44 +0530

    Meme Project

---
+++ Script.js
@@ -0,0 +1,29 @@
+const generateButton = document.querySelector('.generate-button')
+const memeTitle = document.querySelector('.meme-title');
+const memeImage = document.querySelector('.meme-img');
+const authorOutput = document.querySelector('.author');
+
+
+function getMeme()
+{
+    fetch('https://meme-api.com/gimme/wholesomememes')
+    .then((res) => res.json())
+    .then((data)=>{
+        const {author,title,url} = data
+        memeTitle.innerText = title;
+        memeImage.src = url;
+        authorOutput.innerText =` Meme By : ${author}`;
+
+
+        console.log(data);
+        console.log(author,title,url);
+    })
+}
+
+getMeme()
+
+generateButton.addEventListener('click', ()=>{
+
+    getMeme()
+    
+})
